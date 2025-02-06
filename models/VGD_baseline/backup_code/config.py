020a2fc6bd3527d3c189d7cb7507c71ae7d0f446
Momcilo Mrkaic <mmrkaic2-c@my.cityu.edu.hk>
2025-02-06 17:43:08 +0800

    Ignore all .pth files

--- models/VGD_baseline/backup_code/config.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-
-ViSha_training_root = ('D:\\FYP_Momcilo\\Datasets\\GSD-Video-Reflection-Prior\\train', 'video', 'VMD_train')
-ViSha_test_root = ('D:\\FYP_Momcilo\\Datasets\\GSD-Video-Reflection-Prior\\test', 'video', 'VMD_test')
-
--- models/VGD_baseline/backup_code/networks/resnext_modify/config.py
+++
@@ -1,4 +0,0 @@
-# resnext_101_32_path = 'resnext_101_32x4d.pth'
-
-resnext_101_32_path = 'D:\\FYP_Momcilo\VGSD-with-depth\\resnext_101_32x4d.pth'
-
